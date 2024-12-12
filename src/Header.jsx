b9ebc372ef368be79ae73f376d7b2b6434682124
krilinxito <barryallen4207@gmail.com>
2024-12-12 06:19:58 -0400

    cosas

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,16 +4,15 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-    <video className="video-bg" autoPlay loop muted>
-    <video src="/video.mp4" controls />
-      Your browser does not support the video tag.
-    </video>
-    <div className="overlay">
-      <h1>BEST<br />MOVIES</h1>
-      <p>Your favorite movies, revisited and analysed</p>
-    </div>
-  </header>
-  
+      <video className="video-bg" autoPlay loop muted>
+        <source src="/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="overlay">
+        <h1>BEST<br />MOVIES</h1>
+        <p>Your favorite movies, revisited and analyzed</p>
+      </div>
+    </header>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/ProyectoWeb",
+  base: process.env.NODE_ENV === 'production' ? '/ProyectoWeb/' : '/',
   plugins: [react()],
-})
+});
